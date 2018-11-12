879b424c2f7e71f9457c4f24e633836ce0e69a90
Anna Dorsch <Anna@Annas-MacBook-Pro.local>
2018-11-12 20:24:44 +0100

    colors

--- n-Colours/routes/generate.js
+++ n-Colours/routes/generate.js
@@ -28,9 +28,13 @@ const urlOfId = "" + sub + "";
 
 console.log("SUBSTRING", sub);
 console.log("URLOFID", urlOfId);
+let color1;
+let color2 = 100;
+let color3;
 
 let trackId = "";
 let arrayLength = 0;
+
 setTimeout(() => {
   spotifyApi.getPlaylistTracks("" + sub + "").then(data => {
     arrayLength = data.body.items.length;
@@ -46,104 +50,135 @@ setTimeout(() => {
         //console.log("key of our chosen song", data2.body.track.key);
         switch (data2.body.track.key) {
           case 0:
-            console.log("0,100,90");
+            color1 = 0;
+            console.log("color 1", color1);
             break;
           case 1:
-            console.log("30,100,90");
+            color1 = 30;
+            console.log("color 1", color1);
             break;
           case 2:
-            console.log("60,100,90");
+            color1 = 60;
+            console.log("color 1", color1);
             break;
           case 3:
-            console.log("90,100,90");
+            color1 = 90;
+            console.log("color 1", color1);
             break;
           case 4:
-            console.log("120,100,90");
+            color1 = 120;
+            console.log("color 1", color1);
             break;
           case 5:
-            console.log("150,100,90");
+            color1 = 150;
+            console.log("color 1", color1);
             break;
           case 6:
-            console.log("180,100,90");
+            color1 = 180;
+            console.log("color 1", color1);
             break;
           case 7:
-            console.log("210,100,90");
+            color1 = 210;
+            console.log("color 1", color1);
             break;
           case 8:
-            console.log("240,100,90");
+            color1 = 240;
+            console.log("color 1", color1);
             break;
           case 9:
-            console.log("270,100,90");
+            color1 = 270;
+            console.log("color 1", color1);
             break;
           case 10:
-            console.log("300,100,90");
+            color1 = 300;
+            console.log("color 1", color1);
             break;
           case 11:
-            console.log("330,100,90");
+            color1 = 330;
+            console.log("color 1", color1);
             break;
           case 12:
-            console.log("360,100,90");
+            color1 = 360;
+            console.log("color 1", color1);
             break;
         }
+
+        // console.log("COLORS with color 2 is", colors);
         // }
       });
 
-      spotifyApi.getAudioAnalysisForTrack("" + trackId + "").then(data2 => {
-        // console.log(
-        //   "loudness of the song",
-        //   Math.floor(data2.body.track.loudness)
-        // );
-        if (
-          (data2.body.track.key === 0 && data2.body.track.loudness === -1) ||
-          data2.body.track.loudness === 0
-        ) {
-          ("red and bright: 0,100,90");
-        } else if (
-          data2.body.track.key === 0 &&
-          data2.body.track.loudness === -2
-        ) {
-          ("red and less bright: 0,100,82");
-        } else if (
-          data2.body.track.key === 0 &&
-          data2.body.track.loudness === -3
-        ) {
-          ("red and even less bright: 0,100,74");
-        }
+      spotifyApi.getAudioAnalysisForTrack("" + trackId + "").then(
+        data2 => {
+          // console.log(
+          //   "loudness of the song",
+          //   Math.floor(data2.body.track.loudness)
+          // );
+          // if (
+          //   (data2.body.track.key === 0 && data2.body.track.loudness === -1) ||
+          //   data2.body.track.loudness === 0
+          // ) {
+          //   ("red and bright: 0,100,90");
+          // } else if (
+          //   data2.body.track.key === 0 &&
+          //   data2.body.track.loudness === -2
+          // ) {
+          //   ("red and less bright: 0,100,82");
+          // } else if (
+          //   data2.body.track.key === 0 &&
+          //   data2.body.track.loudness === -3
+          // ) {
+          //   ("red and even less bright: 0,100,74");
+          // }
 
-        switch (Math.floor(data2.body.track.loudness)) {
-          case -1:
-            console.log("0,10,90");
-            break;
-          case -2:
-            console.log("0,10,82");
-            break;
-          case -3:
-            console.log("0,10,74");
-            break;
+          switch (Math.floor(data2.body.track.loudness)) {
+            case -1:
+              color3 = 90;
+              console.log("color3", color3);
+              break;
+            case -2:
+              color3 = 82;
+              console.log("color3", color3);
+              break;
+            case -3:
+              color3 = 74;
+              console.log("color3", color3);
+              break;
+            case -4:
+              color4 = 66;
+              console.log("color3", color3);
+              break;
+            case -5:
+              color3 = 58;
+              console.log("color3", color3);
+              break;
+            case -6:
+              color3 = 50;
+              console.log("color3", color3);
+              break;
+            case -7:
+              color3 = 42;
+              console.log("color3", color3);
+              break;
+            case -8:
+              color3 = 34;
+              console.log("color3", color3);
+              break;
+            case -9:
+              color3 = 26;
+              console.log("color3", color3);
+              break;
+            case -10:
+              color3 = 18;
+              console.log("color3", color3);
 
-          case -4:
-            console.log("0,10,66");
-            break;
-          case -5:
-            console.log("0,10,58");
-            break;
-          case -6:
-            console.log("0,10,50");
-            break;
-          case -7:
-            console.log("0,10,42");
-            break;
-          case -8:
-            console.log("0,10,34");
-            break;
-          case -9:
-            console.log("0,10,26");
-            break;
-          case -10:
-            console.log("0,10,18");
-            break;
-        }
-      }, 400);
+              break;
+          }
+          let colors = ` "hsl(${color1}, ${color2 + "%"}, ${color3 + "%"}" )`;
+          console.log("COLORS is", colors);
+        },
+
+        400
+      );
     }
   });
 }, 200);
