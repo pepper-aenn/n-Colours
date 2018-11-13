7f74c5599575fc3d6ed9956f009afaf003cfbc99
Anna Sch <anna-schaeffner@gmx.de>
2018-11-13 17:47:53 +0100

    trz to get ana de

--- n-Colours/routes/generate.js
+++ n-Colours/routes/generate.js
@@ -12,6 +12,7 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 // Remember to paste your credentials here
+ 
 var spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
@@ -31,28 +32,58 @@ router.get("/dashboard", (req, res, next) => {
   res.render("/dashboard");
 });
 
+// router.post("/dashboard", (req, res, next) => {
+//   console.log("--------------------------------------------------");
 
-//getting the url from the form and change it
+//   const playlist_url = req.body.PlaylistID;
+//   url = req.body.PlaylistID;
+
+//   console.log("playlist_url", playlist_url);
+//   console.log("I am the URL", url);
+
+//   if (playlist_url === "") {
+//     res.render({ message: "Indicate Playlist URL" });
+//     return;
+//   }
+
+//   const newPlaylist = new Playlist({
+//     playlist_url
+//   });
+
+//   newPlaylist
+//     .save()
+//     .then(() => {
+//       console.log("SAVED");
+
+//       res.redirect("/generatedArt");
+//     })
+//     .catch(err => {
+//       res.render("/dashboard", { message: "Something went wrong" });
+//     });
+// });
 
 router.post("/dashboard", (req, res, next) => {
   // const playlist_url = req.body.PlaylistID;
   let URL = req.body.PlaylistID;
   var h = URL.split("/")[6];
   var sub = h.substr(0, 22);
-  let urlOfId = sub + "";
+  // let urlOfId = sub + "";
 
+  // console.log("maybe the 6th position of /, who knows:", h);
+  // console.log("SUBSTRING", sub);
+  // console.log("URLOFID", urlOfId);
 
   let trackId = "";
   let arrayLength = 0;
 
   spotifyApi.getPlaylistTracks("" + sub + "").then(data => {
     arrayLength = data.body.items.length;
-    console.log("array length", arrayLength);
+    // console.log("array length", arrayLength);
 
     let promises = [];
 
     for (var i = 0; i < arrayLength; i++) {
-      console.log("Daten von Spotify", data.body.items[i].track.id);
+      // console.log("Daten von Spotify", data.body.items[i].track.id);
       trackId = data.body.items[i].track.id;
       // console.log("gespeichert in einer Variable", trackId);
       promises.push(spotifyApi.getAudioAnalysisForTrack(trackId + ""));
@@ -65,7 +96,7 @@ router.post("/dashboard", (req, res, next) => {
         let color3 = 98 + 8 * Math.floor(datas[i].body.track.loudness);
         colors.push(`hsl(${color1}, ${color2 + "%"}, ${color3 + "%"})`);
       }
-      console.log("COLORS is", colors);
+      // console.log("COLORS is", colors);
       res.render("generatedArt", { colors });
     });
   });
