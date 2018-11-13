688beb0a22801ec1d497e05924808ea32818d96f
Anna Sch <anna-schaeffner@gmx.de>
2018-11-13 18:00:04 +0100

    sdas

--- n-Colours/routes/generate.js
+++ n-Colours/routes/generate.js
@@ -65,6 +65,7 @@ router.get("/dashboard", (req, res, next) => {
 router.post("/dashboard", (req, res, next) => {
   // const playlist_url = req.body.PlaylistID;
   let URL = req.body.PlaylistID;
+  console.log("hello")
   var h = URL.split("/")[6];
   var sub = h.substr(0, 22);
   // let urlOfId = sub + "";
