60654fdd1f60e4a1f4549eb2b31dae8fd4387c73
Anna Dorsch <a.dorsch@ymail.com>
2019-03-23 20:35:04 +0100

    changes

--- passport/index.js
+++ passport/index.js
@@ -1,12 +1,7 @@
 const passport = require("passport");
-const http = require("http");
 
 require("./serializers");
 require("./localStrategy");
-
-setInterval(function() {
-  http.get("http://n-colors.herokuapp.com/");
-}, 300000);
 
 module.exports = app => {
   app.use(passport.initialize());
